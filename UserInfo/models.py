dbbc6f199bc71112098a3df8821eab65be859a60
guodada <261731963@qq.com>
2017-10-05 19:47:30 +0800

    b

--- UserInfo/models.py
+++ UserInfo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from datetime import datetime
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+
+
+class UserProfile(AbstractUser):
+    nick_name = models.CharField(max_length=50)
+    birthday = models.DateField(null=True, blank=True)
+    gender = models.CharField(max_length=20, choices=(('male', 'man'), ('female', 'woman')))
+    address = models.CharField(max_length=100, null=True, blank=True)
+
+    class Meta:
+        verbose_name = 'UserInfo'
+        verbose_name_plural = verbose_name
+
+    def __str__(self):
+        return self.username
