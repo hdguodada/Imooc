14e9932f028e0e6da72ee59e751a75ce04d4ff58
guodada <261731963@qq.com>
2017-10-12 22:28:15 +0800

    b

--- apps/UserInfo/adminx.py
+++ apps/UserInfo/adminx.py
@@ -1,12 +1,33 @@
 import xadmin
 from xadmin import views
 from xadmin.plugins.auth import UserAdmin
-from .models import EmailVerifyRecord, UserProfile
+from .models import EmailVerifyRecord, UserProfile, UserMessage
+
+
+# adminx global setting
+class BaseSetting:
+    enable_themes = True
+    use_bootswatch = True
+
+
+class GlobalSettings:
+    site_title = 'Imooc back'
+    site_footer = 'Imooc'
+    menu_style = 'accordion'
 
 
 class EmailVerifyRecordAdmin:
     list_display = ['code', 'email', 'send_type', 'send_time']
     search_fields = ['code', 'email', 'send_type']
-list_filter = ['code', 'email', 'send_type', 'send_time']
+    list_filter = ['code', 'email', 'send_type', 'send_time']
+
+
+class UserMessageAdmin:
+    list_display = [
+        'user', 'has_read', 'add_time'
+    ]
 
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
+xadmin.site.register(UserMessage, UserMessageAdmin)
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSettings)
